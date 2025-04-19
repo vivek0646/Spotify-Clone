175526c3090636d3507b501572bbbd3599a0f011
vivek <vivek0646.becse24@chitkara.edu.in>
2025-04-19 17:08:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Welcome to Spotify");
+ console.log("Welcome to Spotify");
 
 // Initialize the Variables
 let songIndex = 0;
@@ -14,6 +14,7 @@ let songs = [
     {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
     {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
     {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Blinding Lights-By weekend", filePath: "song/light.mp3", coverpath:"cover/light.jpg"},
     {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
     {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
     {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
